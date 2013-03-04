3c56f98071154c0b94509674913a0451aca27b9c
Daniel Mendel <daniel@danielespeset.com>
2013-03-04 12:17:10 -0500

    Working V0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+Just a lil' server for running HTML experiments locally.
+
+'''
+example $> npm install -g dumbwaiter
+example $> dumbwaiter . 4000
+serving /Users/me/dev/2013/example on port 4000
+'''
---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var connect  = require('connect')
+  , path 	 = require('path')
+  , dirPath  = path.resolve( process.argv[2] )
+  , PORT     = process.argv[3] || 3000
+  , server   = connect()
+
+server.use( connect.static( dirPath )
+server.listen( PORT )
+console.log('serving ' + dirPath + ' on port ' + PORT + ' ...')
